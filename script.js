df888ae1959e2c27f56689c8cebdb3403c2e7081
Edge <eruby.rails@gmail.com>
2021-09-05 22:33:55 -0600

    Code optimization and documentation

--- script.js
+++ script.js
@@ -2,31 +2,33 @@ const counter = document.querySelector('.counter');
 const add = document.querySelector('.increment');
 const subtract = document.querySelector('.reduce');
 
+// Keeps track of count
 let count = 0;
 
+// Updates counter to be equal to the count
 setInterval(() => {
   counter.innerHTML = count;
-  if (count === 0) {
-    counter.style.color = 'black';
-  }
+
+  // Checks out count is zero
+  if (count === 0) counter.style.color = 'black';
 }, 100);
 
 function addToCount() {
   count++;
-  if (count > 0) {
-    counter.style.color = 'green';
-  }
+
+  // Checks if counter is positive
+  if (count > 0) counter.style.color = 'green';
 }
 
 function subtractFromCount() {
   count--;
-  if (count < 0) {
-    counter.style.color = 'red';
-  }
+
+  // Checks if counter is positive
+  if (count < 0) counter.style.color = 'red';
 }
 
+// Returns to zero ( Kore ga requiem... da )
 function returnToZero() {
   count = 0;
-  counter.innerHTML = count;
   counter.style.color = 'black';
 }
